0d364612f50c7df8324e3d6baeb94ec7649d0326
akella <akella.a@gmail.com>
2012-11-02 12:34:00 +0200

    go

---
+++ config.rb
@@ -0,0 +1,28 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+relative_assets = true
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :scss
+module Sass::Script::Functions
+  def image_path(string)
+    assert_type string, :String
+    Sass::Script::String.new("../img/#{string.value}")
+  end
+  alias_method :"image-path",:image_path 
+  declare :"image-path", :args => [:string]
+end
