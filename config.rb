99359a322e7b7b900a953021a97d48526f1d1e0b
akella <akella.a@gmail.com>
2012-11-15 12:22:45 +0200

    minor issues

--- config.rb
+++ config.rb
@@ -10,12 +10,13 @@ relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 preferred_syntax = :scss
 module Sass::Script::Functions
