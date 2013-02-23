981b0ad2f802b555c7321880a03126ecd61c5d3c
akella <akella.a@gmail.com>
2013-02-23 20:14:16 +0200

    cleaning up

--- config.rb
+++ config.rb
@@ -6,19 +6,21 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
-relative_assets = true
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 
 preferred_syntax = :scss
+
+# this one for switching between production and dev versions of the website
 module Sass::Script::Functions
   def image_path(string)
     assert_type string, :String
